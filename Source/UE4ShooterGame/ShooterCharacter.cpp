5d9b78aa6e78720ca1c8ecdf3a9f426ccd34ac22
Lukasz Jastrzebski <luka@lukajast.com>
2024-07-01 21:17:30 +0200

    UE_LOG exercises.

--- Source/UE4ShooterGame/ShooterCharacter.cpp
+++ Source/UE4ShooterGame/ShooterCharacter.cpp
@@ -15,7 +15,27 @@ AShooterCharacter::AShooterCharacter()
 void AShooterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("BeginPlay() has been called"))
 	
+	int MyInt{ 42 };
+	UE_LOG(LogTemp, Warning, TEXT("int MyInt: %d"), MyInt);
+	
+	float MyFloat{ 42.3434554f };
+	UE_LOG(LogTemp, Warning, TEXT("float MyFloat: %f"), MyFloat);
+
+	double MyDouble{ 0.0004 };
+	UE_LOG(LogTemp, Warning, TEXT("double MyDouble: %lf"), MyDouble);
+
+	char MyChar{ 'J' };
+	UE_LOG(LogTemp, Warning, TEXT("char MyChar: %c"), MyChar);
+
+	wchar_t MyWideChar{ L'J' };
+	UE_LOG(LogTemp, Warning, TEXT("wchar_t MyWideChar: %lc"), MyWideChar);
+
+	bool MyBool{ true };
+	UE_LOG(LogTemp, Warning, TEXT("bool MyBool: %d"), MyBool);
+
+	UE_LOG(LogTemp, Warning, TEXT("int: %d, fload %f, bool: %d"), MyInt, MyFloat, MyBool);
 }
 
 // Called every frame
